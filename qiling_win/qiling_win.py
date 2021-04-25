2b49fd0a5ff0da7752bdcd952eed8c1e1f1ca354
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2021-04-25 18:37:10 -0300

    update

--- qiling_win/qiling_win.py
+++ qiling_win/qiling_win.py
@@ -4,12 +4,15 @@ import os
 import sys
 import yara
 import json
+import ctypes
+import struct
 import pefile
+import hexdump
 import logging
 import tempfile
 import argparse
-import ctypes
-import struct
+import capstone
+import hashlib
 from qiling import *
 from qiling.const import *
 from qiling.exception import *
@@ -20,34 +23,29 @@ from qiling.os.windows.handle import *
 from qiling.os.windows.thread import *
 from qiling.os.windows.utils import *
 from qiling.os.windows.structs import *
+
 from karton.core import Karton, Task, Resource
 
 log = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
-
 __author__  = "c3rb3ru5"
 __version__ = "1.0.0"
 
-memory      = []
-dumps       = []
-kernel32    = 'kernel32_dll'
-ntdll       = 'ntdll_dll'
-user32      = 'user32_dll'
+memory       = []
+memory_dumps = []
+kernel32     = 'kernel32_dll'
+ntdll        = 'ntdll_dll'
+user32       = 'user32_dll'
+md32         = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+md64         = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
 
-def dump_executable_memory(self, ql, memory) -> bool:
+def dump_executable_memory(ql) -> bool:
     """
     Dumps Executable Memory
     """
-    if len(memory) > 0:
-        for block in memory:
-            if block['flProtect'] in [PAGE_EXECUTE, PAGE_EXECUTE_READWRITE, PAGE_EXECUTE_WRITECOPY]:
-                try:
-                    dumps.append(ql.mem.read(block['address'], block['size']))
-                    dumps = list(set(dumps))
-                except Exception as error:
-                    log.error(f'unable to read address {address} with size {size}')
-                    False
+    for block in memory:
+        if block['flProtect'] in [PAGE_EXECUTE, PAGE_EXECUTE_READWRITE, PAGE_EXECUTE_WRITECOPY]:
+            memory_dumps.append(ql.mem.read(block['address'], block['size']))
     return True
 
 # Preprocessor Definitions
@@ -81,8 +79,10 @@ class ProcessInformation(WindowsStruct):
         self.dwProcessId = [dwProcessId, self.DWORD_SIZE, 'little', int]
         self.dwThreadId  = [dwThreadId, self.DWORD_SIZE, 'little', int]
 
-def create_hooks(ql):
-    # Create Windows API Hooks
+def hook_apis(ql):
+    """
+    Create Windows API Hooks
+    """
     ql.set_api("VirtualAlloc", hook_VirtualAlloc)
     ql.set_api("VirtualAllocEx", hook_VirtualAllocEx)
     ql.set_api("VirtualProtect", hook_VirtualProtect)
@@ -90,10 +90,11 @@ def create_hooks(ql):
     ql.set_api("memcpy", hook_memcpy)
     ql.set_api("WriteProcessMemory", hook_WriteProcessMemory)
     ql.set_api("Sleep", hook_Sleep)
-    ql.set_api("CreateProcessA", hook_CreateProcessA)
     ql.set_api("IsDebuggerPresent", hook_IsDebuggerPresent)
+    ql.set_api("EnumWindows", hook_EnumWindows)
+    ql.set_api("CopyMemory", hook_CopyMemory)
 
-# Anti-Anti Debug
+# Anti-Anti Debug Function Hooks
 @winsdkapi(cc=STDCALL, dllname=kernel32)
 def hook_Sleep(ql, address, params):
     if params['dwMilliseconds'] > 30000:
@@ -105,29 +106,7 @@ def hook_IsDebuggerPresent(ql, address, params):
     ql.log.info('process called kernel32.IsDebuggerPresent, returning 0')
     return 0
 
-# @winsdkapi(cc=STDCALL, dllname=kernel32)
-# def hook_CreateProcessA(ql, address, params):
-#     CREATE_SUSPENDED = 0x00000004
-#     if params['dwCreationFlags'] & CREATE_SUSPENDED == CREATE_SUSPENDED:
-#         thread_status = QlWindowsThread.READY
-#     else:
-#         thread_status = QlWindowsThread.RUNNING
-#     new_thread = QlWindowsThread(ql)
-#     ql.os.thread_manager.append(thread)
-#     thread_id = new_thread.create(
-#         lpStartAddress,
-#         0x0,
-#         thread_status
-#     )
-#     params['lpProcessInformation'] = ProcessInformation(hProcess=hProcess, hThread=hThread, dwProcessId=CreateProcessId(), dwThreadId=CreateThreadId())
-#     data = {
-#         'library': 'kernel32',
-#         'function': 'CreateProcessA',
-#         'params': params
-#     }
-#     print(json.dumps(data, indent=4))
-
-# Memory Operations
+# Suspicious Memory Functions
 
 @winsdkapi(cc=STDCALL, dllname=kernel32)
 def hook_VirtualAlloc(ql, address, params):
@@ -155,16 +134,6 @@ def hook_WriteProcessMemory(ql, address, params):
         ql.log.exception(error)
         return 0
 
-@winsdkapi(cc=CDECL, dllname=ntdll, replace_params={"dest": POINTER, "src": POINTER, "count": UINT})
-def hook_memcpy(ql, address, params):
-    ql.log.debug(json.dumps({'library': 'ntdll','function': 'memcpy','params': params}))
-    try:
-        data = bytes(ql.mem.read(params['src'], params['count']))
-        ql.mem.write(params['dest'], data)
-    except Exception as e:
-        ql.log.exception("")
-    return params['dest']
-
 @winsdkapi(cc=STDCALL, dllname=kernel32, replace_params_type={'SIZE_T': 'UINT', 'DWORD': 'UINT'})
 def hook_VirtualProtect(ql, address, params):
     memory.append({'address': params['lpAddress'], 'size': params['dwSize'], 'flNewProtect': params['flNewProtect']})
@@ -173,33 +142,73 @@ def hook_VirtualProtect(ql, address, params):
 
 # Memory Extraction Trigger Functions
 
+def hook_asm_x86(ql, address, size):
+    """
+    Hooks Suspicious x86 Assembly Instructions to Trigger Memory Extraction
+    """
+    buf = ql.mem.read(address, size)
+    for i in md32.disasm(buf, address):
+        # Dump Memory on Suspicious Direct Call to EAX
+        if (i.mnemonic == 'call' and i.op_str == 'eax'):
+            dump_executable_memory(ql)
+            ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
+
+def hook_asm_x64(ql, address, size):
+    """
+    Hooks Suspicious x64 Assembly Instructions to Trigger Memory Extraction
+    """
+    buf = ql.mem.read(address, size)
+    for i in md64.disasm(buf, address):
+        # Dump Memory on Suspicious Direct Call to RAX
+        if (i.mnemonic == 'call' and i.op_str == 'rax'):
+            dump_executable_memory(ql)
+            ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
+
+@winsdkapi(cc=CDECL, dllname=ntdll, replace_params={"dest": POINTER, "src": POINTER, "count": UINT})
+def hook_memcpy(ql, address, params):
+    ql.log.debug(json.dumps({'library': 'ntdll','function': 'memcpy','params': params}))
+    try:
+        data = bytes(ql.mem.read(params['src'], params['count']))
+        ql.mem.write(params['dest'], data)
+    except Exception as e:
+        ql.log.exception("")
+    dump_executable_memory(ql)
+    ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
+    return params['dest']
+
+@winsdkapi(cc=STDCALL, dllname=kernel32)
+def hook_CopyMemory(ql, address, params):
+    ql.log.debug(json.dumps({'library': 'kernel32','function': 'CopyMemory','params': params}))
+    try:
+        data = bytes(ql.mem.read(params['Source'], params['Length']))
+        ql.mem.write(params['Destination'], data)
+    except Exception as error:
+        ql.log.error(error)
+    dump_executable_memory(ql)
+    ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
+    return params['Destination']
+
 @winsdkapi(cc=STDCALL, dllname=kernel32)
 def hook_VirtualFree(ql, address, params):
     ql.log.debug(json.dumps({'library': 'kernel32','function': 'VirtualFree','params': params}))
     dumps.append()
-    dump_executable_memory(ql, memory)
-    ql.log.debug('---MEMORY-DUMPS---')
-    ql.log.debug(dumps)
-    ql.log.debug('---MEMORY-DUMPS---')
+    dump_executable_memory(ql)
+    ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
     ql.os.heap.free(params['lpAddress'])
     return 1
 
 @winsdkapi(cc=STDCALL, dllname=kernel32)
 def hook_CreateRemoteThread(ql, address, params):
     ql.log.debug(json.dumps({'library': 'kernel32','function': 'CreateRemoteThread','params': params}))
-    dump_executable_memory(ql, memory)
-    ql.log.debug('---MEMORY-DUMPS---')
-    ql.log.debug(dumps)
-    ql.log.debug('---MEMORY-DUMPS---')
+    dump_executable_memory(ql)
+    ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
     return 1
 
 @winsdkapi(cc=STDCALL, dllname=user32)
 def hook_EnumWindows(ql, address, params):
-    ql.log.debug(json.dumps({'library', 'user32', 'function': 'EnumWindows', 'params': params}))
-    dump_executable_memory(ql, memory)
-    ql.log.debug('---MEMORY-DUMPS---')
-    ql.log.debug(dumps)
-    ql.log.debug('---MEMORY-DUMPS---')
+    ql.log.debug(json.dumps({'library': 'user32', 'function': 'EnumWindows', 'params': params}))
+    dump_executable_memory(ql)
+    ql.log.debug('MEMORY_DUMP_COUNT: {memory_dump_count}'.format(memory_dump_count=len(memory_dumps)))
     return 1
 
 yara_rule_upx = """
@@ -231,8 +240,18 @@ class KartonUnpackerModule():
     Unpacks Executables that Create Executable Memory using the Qiling Framework
     """
 
-    def __init__(self, sample) -> None:
+    def __init__(self, sample, config) -> None:
         self.enabled = self.yara_check(sample)
+        self.config = config
+        if self.config['rootfs'] is None:
+            log.warn("rootfs is disabled, qiling_win unpacker disabled")
+            self.enabled = False
+        if self.config['debug'] is True:
+            logging.basicConfig(level=logging.DEBUG)
+            self.verbose = 4
+        else:
+            logging.basicConfig(level=logging.INFO)
+            self.verbose = 0
 
     def yara_check(self, sample) -> bool:
         self.data = sample.content
@@ -254,6 +273,38 @@ class KartonUnpackerModule():
         f.close()
         return sample_packed
 
+    def get_tasks(self):
+        global memory_dumps
+        self.memory_dump_cleanup()
+        tasks = []
+        if len(memory_dumps) <= 0:
+            return tasks
+        print("Successfully Extracted {memory_dumps_count} Suspicious Memory Dump(s)".format(memory_dumps_count=len(memory_dumps)))
+        for memory_dump in memory_dumps:
+            if self.config['debug'] is True:
+                log.debug('EXTRACTED_PAYLOAD:')
+                hexdump.hexdump(memory_dump)
+            tasks.append(Task(
+                headers={
+                    'type': 'sample',
+                    'kind': 'raw'
+                },
+                payload={
+                    'parent': Resource(name='sample', content=self.data),
+                    'sample': Resource(name='unpacked', content=memory_dump)
+                }
+            ))
+        return tasks
+
+    def memory_dump_cleanup(self):
+        """
+        Converts Bytearrays to Bytes and Dedups Array
+        """
+        global memory_dumps
+        for i in range(0, len(memory_dumps)):
+            memory_dumps[i] = bytes(memory_dumps[i])
+        memory_dumps = list(set(memory_dumps))
+
     def main(self) -> Task:
         sample_packed = self.write_sample_tempfile()
         pe = pefile.PE(sample_packed)
@@ -263,14 +314,15 @@ class KartonUnpackerModule():
                 log.info(f"starting analysis of win32 executable {self.name}")
                 ql = Qiling(
                     argv=[sample_packed],
-                    rootfs='/home/c3rb3ru5/Tools/titan-temufs' + '/x86_windows',
+                    rootfs=self.config['rootfs'] + '/x86_windows',
                     multithread=False,
                     console=False,
                     log_override=log,
-                    verbose=4
+                    verbose=self.verbose
                 )
-                create_hooks(ql)
-                ql.run(timeout=50000)
+                hook_apis(ql)
+                ql.hook_code(hook_asm_x86)
+                ql.run(timeout=self.config['timeout'])
             except Exception as error:
                 log.error(error)
         if hex(pe.FILE_HEADER.Machine) == '0x8664':
@@ -279,46 +331,64 @@ class KartonUnpackerModule():
                 log.info(f"starting analysis of win64 executable {self.name}")
                 ql = Qiling(
                     argv=[sample_packed],
-                    rootfs='/home/c3rb3ru5/Tools/titan-temufs' + '/x86_windows',
+                    rootfs=self.config['rootfs'] + '/x86_windows',
                     multithread=False,
                     console=False,
                     log_override=log,
-                    verbose=0
+                    verbose=self.verbose
                 )
-                create_hooks(ql)
-                ql.run(timeout=5000)
+                hook_apis(ql)
+                ql.hook_code(hook_asm_x64)
+                ql.run(timeout=self.config['timeout'])
             except Exception as error:
                 log.error(error)
-        return None
-
-# return Task(
-#     headers={
-#         "type": "sample",
-#         "kind": "raw"
-#     },
-#     payload={
-#         "parent": Resource(name='sample', content=self.data),
-#         "sample": child_resource
-#     }
-# )
-        
+        return self.get_tasks()
+  
 if __name__ in '__main__':
-    logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(
         prog='qiling_win.py',
         description=f'Karton Windows Unpacker Service module v{__version__} powered by Qiling Framework (CLI Test Utility)',
         epilog=f'Author: {__author__}'
     )
-    parser.add_argument('-i','--input', help='Input File', type=str, required=True)
-    parser.add_argument('-t', '--timeout', help="Timeout", type=int, default=2000, required=False)
-    parser.add_argument('-d', '--debug', help='Debug', action='store_true', default=False, required=False)
-    parser.add_argument('-r', '--rootfs', help='RootFS', type=str, default=None, required=False)
+    parser.add_argument('--input', help='Input File', type=str, required=True)
+    parser.add_argument('--timeout', help="Timeout", type=int, default=5000, required=False)
+    parser.add_argument('--debug', help='Debug', action='store_true', default=True, required=False)
+    parser.add_argument('--rootfs', help='RootFS', type=str, default=None, required=True)
     args = parser.parse_args()
+    config = {
+            'rootfs': args.rootfs,
+            'timeout': args.timeout,
+            'debug': args.debug
+    }
     f = open(args.input, 'rb')
     sample = Resource(name=args.input, content=f.read())
     f.close()
-    module = KartonUnpackerModule(sample)
+    module = KartonUnpackerModule(sample=sample, config=config)
     if module.enabled is True:
-        task = module.main()
-        # data = json.loads(str(task))
-        # print(json.dumps(data, indent=4))
+        tasks = module.main()
+        for task in tasks:
+            data = json.loads(str(task))
+            print(json.dumps(data, indent=4))
+
+# Works in Progress
+# @winsdkapi(cc=STDCALL, dllname=kernel32)
+# def hook_CreateProcessA(ql, address, params):
+#     CREATE_SUSPENDED = 0x00000004
+#     if params['dwCreationFlags'] & CREATE_SUSPENDED == CREATE_SUSPENDED:
+#         thread_status = QlWindowsThread.READY
+#     else:
+#         thread_status = QlWindowsThread.RUNNING
+#     new_thread = QlWindowsThread(ql)
+#     ql.os.thread_manager.append(thread)
+#     thread_id = new_thread.create(
+#         lpStartAddress,
+#         0x0,
+#         thread_status
+#     )
+#     params['lpProcessInformation'] = ProcessInformation(hProcess=hProcess, hThread=hThread, dwProcessId=CreateProcessId(), dwThreadId=CreateThreadId())
+#     data = {
+#         'library': 'kernel32',
+#         'function': 'CreateProcessA',
+#         'params': params
+#     }
+#     print(json.dumps(data, indent=4))
